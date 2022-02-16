6f3d569becc6d9e313f453ecf0e8bedd701d8150
palio19888 <pauloroberto30051988@outlook.com>
2022-02-15 21:14:07 -0300

    trabalho de array

---
+++ array.js
@@ -0,0 +1,25 @@
+/*clearconst lista = new Array("pera","uva","maça")
+
+const primeiroItem = lista[0]
+
+const tamanho = lista.lenght
+
+console.log(tamanho)*/
+
+
+const lista =[
+{nome: "davison",curso: "front_end", matricula: "002"},
+{nome: "saulo",curso: "beck-end",matricula: "003"},
+{nome: "sandra",curso: "desenvolvimento web", matricula: "004"},
+{nome: "rivaldo",curso: "banco de dados", matricula: "005"},
+{nome: "charliton",curso: "banco de dados", matricula: "006"},
+{nome: "tereza", curso: "banco de dados", matricula: "007"}
+]
+
+const consulta = lista [1]
+
+
+
+console.log(consulta)
+
+
